6060752532384c2f43afaf86038daa211628b910
Nekindness <137820039+Nekindness@users.noreply.github.com>
2024-04-27 19:09:15 +0300

    App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,45 +1,43 @@
-import React, { useState } from 'react'; // Импорт React и хука useState из библиотеки React
-import './App.css'; // Импорт стилей из файла App.css
+import React, { useState } from 'react';
+import './App.css';
 
-// Компонент ProductList, отображающий список продуктов
 const ProductList = ({ products, onDelete, onIncrement, onDecrement, onAdd }) => {
   return (
-    <div className="product-list"> {/* Обертка для списка продуктов */}
-      {products.map((product) => ( // Итерация по каждому продукту в списке
-        <div key={product.id} className="product-item"> {/* Элемент для отдельного продукта */}
-          <div>{product.name}</div> {/* Отображение названия продукта */}
-          <div>Цена: ₽{product.price * product.count}</div> {/* Отображение цены продукта */}
-          <div>Количество: {product.count}</div> {/* Отображение количества продукта */}
-          <div className="product-buttons"> {/* Обертка для кнопок управления количеством */}
-            <button onClick={() => onDecrement(product.id)}>-</button> {/* Кнопка уменьшения количества продукта */}
-            <button onClick={() => onIncrement(product.id)}>+</button> {/* Кнопка увеличения количества продукта */}
+    <div className="product-list">
+      {products.map((product) => (
+        <div key={product.id} className="product-item">
+          <div>{product.name}</div>
+          <div>Цена: ₽{product.price * product.count}</div>
+          <div>Количество: {product.count}</div>
+          <div className="product-buttons">
+            <button onClick={() => onDecrement(product.id)}>-</button>
+            <button onClick={() => onIncrement(product.id)}>+</button>
           </div>
-          <div className="delete-button" onClick={() => onDelete(product.id)}> {/* Кнопка удаления продукта */}
+          <div className="delete-button" onClick={() => onDelete(product.id)}>
             Удалить товар
           </div>
         </div>
       ))}
-      <div className="add-button" onClick={onAdd}> {/* Кнопка добавления нового товара */}
+      <div className="add-button" onClick={onAdd}>
         Добавить новый товар
       </div>
     </div>
   );
 };
 
-// Компонент App, основной компонент приложения
 const App = () => {
-  const [products, setProducts] = useState([ // Инициализация состояния products с помощью хука useState
-    { id: 1, name: 'Велосипед', price: 1000, count: 1 }, // Начальные данные о продуктах
+  const [products, setProducts] = useState([ 
+    { id: 1, name: 'Велосипед', price: 1000, count: 1 },
     { id: 2, name: 'Самокат', price: 3500, count: 1 },
     { id: 3, name: 'Ролики', price: 2000, count: 1 },
     { id: 4, name: 'Сноуборд', price: 15000, count: 1 }
   ]);
 
-  const handleDelete = (id) => { // Функция для удаления продукта
-    setProducts(products.filter((product) => product.id !== id)); // Удаление продукта из списка
+  const handleDelete = (id) => {
+    setProducts(products.filter((product) => product.id !== id));
   };
 
-  const handleIncrement = (id) => { // Функция для увеличения количества продукта
+  const handleIncrement = (id) => { 
     setProducts(
       products.map((product) =>
         product.id === id ? { ...product, count: Math.min(product.count + 1, 25) } : product
@@ -47,7 +45,7 @@ const App = () => {
     );
   };
 
-  const handleDecrement = (id) => { // Функция для уменьшения количества продукта
+  const handleDecrement = (id) => {
     setProducts(
       products.map((product) =>
         product.id === id ? { ...product, count: Math.max(product.count - 1, 0) } : product
@@ -55,22 +53,22 @@ const App = () => {
     );
   };
 
-  const handleAdd = () => { // Функция для добавления нового продукта
-    const newId = products.length + 1; // Генерация нового идентификатора для продукта
-    const newName = prompt('Введите название нового товара:'); // Запрос названия нового товара
-    const newPrice = Number(prompt('Введите цену нового товара:')); // Запрос цены нового товара
-    const newCount = Number(prompt('Введите количество нового товара:')); // Запрос количества нового товара
-    setProducts([...products, { id: newId, name: newName, price: newPrice, count: newCount }]); // Добавление нового продукта в список
+  const handleAdd = () => {
+    const newId = products.length + 1;
+    const newName = prompt('Введите название нового товара:');
+    const newPrice = Number(prompt('Введите цену нового товара:'));
+    const newCount = Number(prompt('Введите количество нового товара:'));
+    setProducts([...products, { id: newId, name: newName, price: newPrice, count: newCount }]);
   };
 
   return (
     <div className="app">
       <ProductList
-        products={products} //список продуктов, который будет отображаться в компоненте ProductList.
-        onDelete={handleDelete} //функция обработчик, которая будет вызываться при удалении продукта из списка.
-        onIncrement={handleIncrement} //функция обработчик, которая будет вызываться при увеличении количества продукта.
-        onDecrement={handleDecrement} // функция обработчик, которая будет вызываться при уменьшении количества продукта.
-        onAdd={handleAdd} //функция обработчик, которая будет вызываться при добавлении нового продукта в список.
+        products={products}
+        onDelete={handleDelete}
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+        onAdd={handleAdd}
       />
     </div>
   );
